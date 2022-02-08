77fd126638cd9f022c9605237c5a9881dbdda6a5
Nymph <quantnymph@gmail.com>
2022-02-07 23:37:35 -0600

    add curl to setup.sh apt install

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 
 sudo apt update
 sudo apt upgrade
-sudo apt install -y nodejs npm
+sudo apt install -y nodejs npm curl
 sudo npm install -g npm@latest
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
